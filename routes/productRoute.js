a9c4ded9a97b690d439ee12324e7e5038184d11d
johndakshak <johndakshak@gmail.com>
2026-07-13 22:11:06 +0100

    feat: implement delete product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addProduct, getProducts, getProductById, updateProduct } from "../controller/productController";
+import { addProduct, getProducts, getProductById, updateProduct, deleteProduct } from "../controller/productController";
 import { authMiddleware } from "../middleware/authMiddleware";
 import { adminMiddleware } from "../middleware/adminMiddleware";
 import { upload } from "../config/cloudinary";
@@ -10,5 +10,6 @@ router.post("/product", authMiddleware, adminMiddleware, upload.single("image"),
 router.get("/products", getProducts);
 router.get("/product/:id", getProductById);
 router.patch("/product/:id", authMiddleware, adminMiddleware, upload.single("image"), updateProduct);
+router.delete("/product/:id", authMiddleware, adminMiddleware, deleteProduct);
 
 export default router;
