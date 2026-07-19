7180ecc547e0da9b7919193a29ed1b875943e04a
johndakshak <johndakshak@gmail.com>
2026-07-20 00:01:57 +0100

    feat: implement rate limiting

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { login } from "../controller/loginController";
+import { loginLimiter } from "../middleware/rateLimitMiddleware";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/login", loginLimiter, login);
 
 export default router;
